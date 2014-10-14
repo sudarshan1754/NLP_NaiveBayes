75e485b5b2461bd9a020c7496683157eee647d7e
Sid <shetty.sud@gmail.com>
2014-10-14 15:06:07 -0500

    Added Cross Validation Split Code

---
+++ NaiveBayes.py
@@ -0,0 +1,89 @@
+###********************************************************************************###
+  # __author__ = 'sid'                                                             #
+  # This program is written as part of the Natural Language Processing Home Work 4 #
+  # @copyright: Sudarshan Sudarshan (Sid)                                          #
+###********************************************************************************###
+
+import sys
+import os
+
+
+class PreProcess:
+
+    @staticmethod
+    def read_file_names(directory_path):
+        files = []
+        for each_file in os.listdir(directory_path):
+            files.append(each_file)
+
+        return sorted(files)
+
+    # split the file names
+    @staticmethod
+    def split_file_names(file_list, folds, group_of):
+
+        # print no_of_test_files, no_of_train_files
+        i = 0
+        group = group_of
+        splits_list = []
+        for each_fold in range(0, folds):
+            # list to store the train and test split
+            model = []
+
+            #get the test data
+            test = []
+            for each in range(i, group):
+                test.append(file_list[each])
+
+            # get the training data
+            train = [x for x in file_list if x not in test]
+
+            # store train and test in a list
+            model.append(train)
+            model.append(test)
+
+            #store the fold in the list
+            splits_list.append(model)
+
+            #increment for next fold
+            i += group_of
+            group += group_of
+
+        return splits_list
+
+    # get the file name list and do cross validation
+    def cross_validation(self, files_list, folds):
+
+        no_of_test_files = len(files_list) / folds
+
+        split_data = self.split_file_names(files_list, folds, no_of_test_files)
+
+        cross_file = open("cross", "w")
+        for each in split_data:
+            # print each
+            cross_file.write(str(each) + "\n")
+        return split_data
+
+
+if __name__ == "__main__":
+    pos_dir = raw_input('Enter the directory path which has positive files:')
+    neg_dir = raw_input('Enter the directory path which has negative files:')
+
+    if len(pos_dir) == 0 or len(neg_dir) == 0:
+        print "Enter the valid path"
+        sys.exit()
+
+    pre = PreProcess()
+
+    pos_file_Names = pre.read_file_names(pos_dir)
+    neg_file_Names = pre.read_file_names(neg_dir)
+
+    no_of_folds = 10
+    pos_split = pre.cross_validation(pos_file_Names, no_of_folds)
+    neg_split = pre.cross_validation(neg_file_Names, no_of_folds)
+
+
+    # print len(pos_split)
+    # print len(neg_split)
+
+
